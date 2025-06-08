6d7b28be253c43e99d8e81defbd9474e6e4778bc
Lee jeongchan <sjk338285@gmail.com>
2025-06-08 20:20:07 +0900

    feat : 프로젝트 기본 구조 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
+import { BaseballModule } from './baseball/baseball.module';
+import { HighlightModule } from './highlight/highlight.module';
+import { ScheduleModule } from './schedule/schedule.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [UserModule, PostModule, AuthModule],
+  imports: [UserModule, PostModule, AuthModule, BaseballModule, HighlightModule, ScheduleModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
