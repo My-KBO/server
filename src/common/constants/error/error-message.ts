c3e9ad3a6249ba5b5e6d18a107f1ac6e1b644ace
Lee jeongchan <sjk338285@gmail.com>
2025-06-18 21:23:25 +0900

    feat : DTO 및 상수 수정

--- src/common/constants/error/error-message.ts
+++ src/common/constants/error/error-message.ts
@@ -10,6 +10,7 @@ export const ErrorMessage = {
     NO_PERMISSION: '게시글을 수정하거나 삭제할 권한이 없습니다.',
     POST_ALREADY_LIKED: '이미 게시글에 좋아요를 눌렀습니다.',
     POST_LIKE_NOT_FOUND: '해당 게시글에 대한 좋아요 정보를 찾을 수 없습니다.',
+    ACCESS_DENIED: '게시글 접근 권한이 없습니다.',
   },
 
   Comment: {
--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,4 +1,5 @@
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { PostCategory } from 'src/common/constants/post-category.enum';
 
 export class CreatePostDto {
   @IsOptional()
