b59e36d2ebe59140a3d21414a1f93d7c25e56076
Lee jeongchan <sjk338285@gmail.com>
2025-06-19 16:21:56 +0900

    Update create-post.dto.ts

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -2,17 +2,14 @@ import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { PostCategory } from 'src/common/constants/post-category.enum';
 
 export class CreatePostDto {
-  @IsOptional()
   @IsString()
   @IsNotEmpty()
   title: string;
 
-  @IsOptional()
   @IsString()
   @IsNotEmpty()
   content: string;
 
-  @IsOptional()
   @IsEnum(PostCategory)
   category: PostCategory;
 }
