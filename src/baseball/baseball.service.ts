cf17dc7898cc76dfcde5ef6a9fa3cbac4a390d40
Lee jeongchan <sjk338285@gmail.com>
2025-07-01 18:02:14 +0900

    feat : 서비스 로직 구현

--- src/baseball/baseball.service.ts
+++ src/baseball/baseball.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class BaseballService {}
+export class BaseballService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getTeamRankings(): Promise<GetTeamRankingResponseDto[]> {
+    const data = await this.prisma.teamRanking.findMany({ orderBy: { rank: 'asc' } });
+    return data;
+  }
+
+  async getTopPlayers(): Promise<GetTopPlayerResponseDto[]> {
+    const hitters = await this.prisma.playerStat.findMany({
+      where: { category: '타자' },
+      orderBy: { value: 'desc' },
+      take: 5,
+    });
+
+    const pitchers = await this.prisma.playerStat.findMany({
+      where: { category: '투수' },
+      orderBy: { value: 'asc' },
+      take: 5,
+    });
+
+    return [...hitters, ...pitchers];
+  }
+
+  async getTodayGames(): Promise<GetTodayGameResponseDto[]> {
+    const todayStr = new Date().toISOString().slice(0, 10);
+    return await this.prisma.schedule.findMany({
+      where: { date: todayStr },
+      orderBy: { time: 'asc' },
+    });
+  }
+}
