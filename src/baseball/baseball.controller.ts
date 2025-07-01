99a1a1558b66a5a11bb3bb5ffad858308a6c382b
Lee jeongchan <sjk338285@gmail.com>
2025-07-01 18:01:23 +0900

    feat : 엔드포인트 구현

--- src/baseball/baseball.controller.ts
+++ src/baseball/baseball.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { BaseballService } from './baseball.service';
 
-@Controller('baseball')
-export class BaseballController {}
+@ApiTags('baseball')
+@Controller('api/v1/baseball')
+export class BaseballController {
+  constructor(private readonly baseballService: BaseballService) {}
+
+  @ApiOperation({ summary: '전체 팀 순위' })
+  @ApiResponse({ status: 200, type: [GetTeamRankingResponseDto] })
+  @Get('rankings')
+  async getTeamRankings() {
+    return this.baseballService.getTeamRankings();
+  }
+
+  @ApiOperation({ summary: '플레이어 순위 (타자/투수)' })
+  @ApiResponse({ status: 200, type: [GetTopPlayerResponseDto] })
+  @Get('topplayer')
+  async getTopPlayers() {
+    return this.baseballService.getTopPlayers();
+  }
+
+  @ApiOperation({ summary: '오늘의 경기 일정' })
+  @ApiResponse({ status: 200, type: [GetTodayGameResponseDto] })
+  @Get('games/today')
+  async getTodayGames() {
+    return this.baseballService.getTodayGames();
+  }
+}
