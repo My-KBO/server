5d53cce1964e8dac1541a71a0dd8fdc515316c6e
Lee jeongchan <sjk338285@gmail.com>
2025-07-03 23:01:13 +0900

    feat : 의존성 추가

--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NewsController } from './news.controller';
 import { NewsService } from './news.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [NewsController],
-  providers: [NewsService]
+  providers: [NewsService],
 })
 export class NewsModule {}
