8036c1abedc92a844e9c88adbeffcdcbde455406
Lee jeongchan <sjk338285@gmail.com>
2025-07-08 21:30:03 +0900

    feat : 하이라이트 로직 구현

--- src/highlight/highlight.service.ts
+++ src/highlight/highlight.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+const YOUTUBE_CHANNEL_ID = 'UCoVz66yWHzVsXAFG8WhJK9g';
 
 @Injectable()
-export class HighlightService {}
+export class HighlightService {
+  private baseUrl = 'https://www.googleapis.com/youtube/v3';
+
+  async searchHighlights(query: string, maxResults = 5) {
+    const res = await axios.get(`${this.baseUrl}/search`, {
+      params: {
+        part: 'snippet',
+        q: `${query} 하이라이트`,
+        key: YOUTUBE_API_KEY,
+        channelId: YOUTUBE_CHANNEL_ID,
+        maxResults,
+        type: 'video',
+        order: 'date',
+      },
+    });
+
+    return res.data.items.map((item: any) => ({
+      title: item.snippet.title,
+      publishedAt: item.snippet.publishedAt,
+      videoUrl: `https://www.youtube.com/watch?v=${item.id.videoId}`,
+      thumbnail: item.snippet.thumbnails.high.url,
+    }));
+  }
+}
